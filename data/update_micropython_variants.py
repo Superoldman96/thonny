5c72706b8549e93e48b24030d8d28215626bf5eb
Aivar Annamaa <aivarannamaa@users.noreply.github.com>
2026-09-23 09:50:42 +0300

    MP 1.29

--- data/update_micropython_variants.py
+++ data/update_micropython_variants.py
@@ -12,9 +12,9 @@ from update_variants_common import (
 
 base_url = "https://micropython.org/download/"
 
-UNSTABLE_VERSION = r"\d{8}-v1.29.0-preview\.\d+\.[a-z0-9]{10}"
-PREV_RELEVANT_VERSION = "1.27.0"
-PREV_RELEVANT_VERSION_IN_URL = "20251209-v1.27.0"
+UNSTABLE_VERSION = r"\d{8}-v1.30.0-preview\.\d+\.[a-z0-9]{10}"
+PREV_RELEVANT_VERSION = "1.28.0"
+PREV_RELEVANT_VERSION_IN_URL = "20260406-v1.28.0"
 
 
 class IndexParser(HTMLParser):
